307544daafafccff9b6072992b7973623d15ff9b
swarupmandal <swarup12x@gmail.com>
2022-06-28 22:12:59 +0530

    first commit

---
+++ src/test/java/com/sm/jenkins_pipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.jenkins_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
